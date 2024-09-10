d56da0a86f9424f9d64a42901b483fb3297f34f6
YahyaNashar22 <yahyanashar22@gmail.com>
2024-09-10 14:40:57 +0300

    addded essential setup for the server, models for category, order, product, review, user

---
+++ server/models/orderModel.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const { Schema, model } = mongoose;
+
+const orderSchema = new Schema({
+    status: {
+        type: Boolean,
+        required: true
+    },
+    totalPrice: {
+        type: Number,
+        required: true,
+    },
+    products: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Product',
+        required: true
+    }],
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+})
+
+const Order = model("Order", orderSchema);
+export default Order;
---
+++ server/server.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import bodyParser from 'body-parser';
+
+import dbConnection from './utils/dbConnection.js';
+
+dotenv.config();
+const app = express();
+
+//CORS Policies
+app.use(cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+    optionsSuccessStatus: 200,
+}
+));
+
+//essential middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Routes for managing models
+
+
+app.listen(process.env.PORT, (error) => {
+    if (!error) {
+        console.log(`Server running on port: ${process.env.PORT}`);
+    } else {
+        console.log(`Error: ${error}`);
+    }
+});
+dbConnection();
